ab7414d64686032392e7e6a9e05c60485a5c63f2
WindingDuke77 <64500127+WindingDuke77@users.noreply.github.com>
2021-04-18 20:26:30 +0100

    save 1

--- lua/entities/sent_sortinghat/init.lua
+++ lua/entities/sent_sortinghat/init.lua
@@ -4,12 +4,17 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-    self:SetModel("models/props_c17/tools_wrench01a.mdl")
+    self:SetModel("models/sortinghat/sortinghat.mdl")
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS) 
     self:SetSolid(SOLID_VPHYSICS)
     self:SetUseType(SIMPLE_USE)
     self:SetTrigger(true)
+
+    self.Gryffindor = 25
+    self.Ravenclaw = 25
+    self.Hufflepuff = 25
+    self.Slytherin = 25
 
     local phys = self:GetPhysicsObject()
     if phys:IsValid() then
@@ -21,7 +26,13 @@ function ENT:Initialize()
     end
 end
 
-
+--  Gryffindor, Ravenclaw, Hufflepuff or Slytherin.
 function ENT:Use(act, call)
+    self.total = self.Gryffindor + self.Ravenclaw + self.Hufflepuff + self.Slytherin 
     
+    self.number = math.random(0, 100)
+    if number < Gryffindor then
+        self:ChatPrint("You belong in Gryffindor")
+    
+
 end
