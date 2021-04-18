f59fccdf2ae2061e3873770eb1dc989b25e42a9d
WindingDuke77 <64500127+WindingDuke77@users.noreply.github.com>
2021-04-18 21:34:43 +0100

    Garrymod first version

--- lua/entities/sent_sortinghat/init.lua
+++ lua/entities/sent_sortinghat/init.lua
@@ -3,6 +3,16 @@ AddCSLuaFile("shared.lua")
 
 include("shared.lua")
 
+local Hatvoicelines = { -- you can add more voice lines here, 
+
+    "I see, not a bad mind",
+    "There's talent, oh yes",
+    "Not a bad mind",
+    "Hmm, difficult. VERY difficult",
+    "There's talent, oh yes"  -- Remember you dont need a comma at the last one
+
+}
+
 function ENT:Initialize()
     self:SetModel("models/sortinghat/sortinghat.mdl")
     self:PhysicsInit(SOLID_VPHYSICS)
@@ -11,11 +21,14 @@ function ENT:Initialize()
     self:SetUseType(SIMPLE_USE)
     self:SetTrigger(true)
 
-    self.Gryffindor = 25
+    self.Gryffindor = 25 -- change these to change the chance
     self.Ravenclaw = 25
     self.Hufflepuff = 25
     self.Slytherin = 25
-    self.time = CurTime()
+
+
+
+    self.active = true
 
     local phys = self:GetPhysicsObject()
     if phys:IsValid() then
@@ -27,29 +40,46 @@ function ENT:Initialize()
     end
 end
 
---  Gryffindor, Ravenclaw, Hufflepuff or Slytherin.
+
 function ENT:Use(act, call)
-    self.total = self.Gryffindor + self.Ravenclaw + self.Hufflepuff + self.Slytherin 
+    if self.active then
+        self.active = false
+        self.total = self.Gryffindor + self.Ravenclaw + self.Hufflepuff + self.Slytherin  
 
-    self.Gryffindorchance = self.Gryffindor / self.total * 100
-    self.Ravenclawchance = self.Ravenclaw / self.total * 100 + self.Gryffindorchance
-    self.Hufflepuffchance = self.Hufflepuff / self.total * 100 + self.Ravenclawchance
-    self.Slytherinchance = self.Slytherin / self.total * 100 + self.Hufflepuffchance
+        self.Gryffindorchance = self.Gryffindor / self.total * 100
+        self.Ravenclawchance = self.Ravenclaw / self.total * 100 + self.Gryffindorchance
+        self.Hufflepuffchance = self.Hufflepuff / self.total * 100 + self.Ravenclawchance
+        self.Slytherinchance = self.Slytherin / self.total * 100 + self.Hufflepuffchance
 
-    call:ChatPrint("Hmm, difficult. VERY difficult")
+        self.linescount = 0
+        call:ChatPrint("Hmmmm let me see") -- First thing he says
 
-    coroutine.wait( 5 )
+        timer.Create( "UniqueName1", 3, 2, function() 
+            call:ChatPrint(Hatvoicelines[math.random(1, table.getn(Hatvoicelines))]) 
 
+            self.linescount = self.linescount + 1
+            if self.linescount == 2 then
+                self.number = math.random(0, 100)
 
-    self.number = math.random(0, 100)
-
-    if self.number <  self.Gryffindorchance then
-        call:ChatPrint("You belong in Gryffindor, " .. call:Nick())
-    elseif self.number < self.Ravenclawchance then
-        call:ChatPrint("You belong in Ravenclaw, " .. call:Nick())
-    elseif self.number < self.Hufflepuffchance then
-        call:ChatPrint("You Belong in Hufflepuff, " .. call:Nick())
-    elseif self.number < self.Slytherinchance then
-        call:ChatPrint("You Belong in Slytherin, " .. call:Nick())
+                if self.number <  self.Gryffindorchance then
+                    call:ChatPrint("You belong in Gryffindor, " .. call:Nick())
+                    self:EmitSound("gryffindor.wav")
+                    self.active = true
+                elseif self.number < self.Ravenclawchance then
+                    call:ChatPrint("You belong in Ravenclaw, " .. call:Nick())
+                    self:EmitSound("ravenclaw.wav")
+                    self.active = true
+                elseif self.number < self.Hufflepuffchance then
+                    call:ChatPrint("You Belong in Hufflepuff, " .. call:Nick())
+                    self:EmitSound("hufflepuff.wav")
+                    self.active = true
+                elseif self.number < self.Slytherinchance then
+                    call:ChatPrint("You Belong in Slytherin, " .. call:Nick())
+                    self:EmitSound("slytherin.wav")
+                    self.active = true
+                end
+            end
+        end)
     end
+
 end
